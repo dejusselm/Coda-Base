184f9292098d08cbd216f5a17c15c99f24dace37
dejusselm <dejusselm@gmail.com>
2025-10-02 11:53:57 +0200

    Exercice n°4

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def exercice2():
 def exercice3():
     print("Bonjour \n le \n monde")
 
+def exercice4():
+    annee = int(input('Quelle est votre année de naissance ? '))
+    print("Vous avez environ ",2025-annee," ans")
+
+
+
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
@@ -18,8 +24,12 @@ def main():
         exercice2()
     elif choix == "3":
         exercice3()
+    elif choix == "4":
+        exercice4()
+        
     else:
         print("Exercice non reconnu.")
+
 
 
 if __name__ == "__main__":
