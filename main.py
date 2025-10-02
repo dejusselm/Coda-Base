3142b7e4b27ef777a16e92a5261b1560534fc270
dejusselm <dejusselm@gmail.com>
2025-10-02 11:50:58 +0200

    Exercices 2 et 3

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ def exercice1():
     print("Exercice 1 : Bonjour le monde !")
     print("Hello World !")
 
+def exercice2():
+    nom = input('Quel est votre nom ? ')
+    print("Bonjour ",nom," !")
+
+def exercice3():
+    print("Bonjour \n le \n monde")
+
 def main():
     # Demande à l'utilisateur quel exercice exécuter
     choix = input("Entrez le numéro de l'exercice à exécuter : ")
     if choix == "1":
         exercice1()
+    elif choix == "2":
+        exercice2()
+    elif choix == "3":
+        exercice3()
     else:
         print("Exercice non reconnu.")
 
